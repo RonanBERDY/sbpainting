67c1a6e579d75aa2efcecb50a55e290829c244db
RonanBERDY <ronan.berdy3@etu.univ-lorraine.fr>
2025-01-26 21:37:44 +0100

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthModelComponent } from './user/auth-model/auth-model.component';
+import { NavComponent } from './core/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AuthModelComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/user/auth-model/auth-model.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ModelComponent } from '../../shared/model/model.component';
+@Component({
+  selector: 'app-auth-model',
+  imports: [ModelComponent],
+  templateUrl: './auth-model.component.html',
+  styleUrl: './auth-model.component.css'
+})
+export class AuthModelComponent {
+
+}
