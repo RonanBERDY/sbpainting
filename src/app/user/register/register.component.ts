df6d55ca6d7b0377aeefccb9cc55d0e4ec011729
RonanBERDY <ronan.berdy3@etu.univ-lorraine.fr>
2025-02-01 19:00:57 +0100

    bindings

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms'; //formbuilder c le container de form
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  fb=inject(FormBuilder);
+  form=this.fb.nonNullable.group({
+    name:[''],email:[''],password:[''],confirm_password:[''],//if age age:[18]
+  });
 }
