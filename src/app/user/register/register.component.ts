ce965889cd905781a41dbb7c1e75c59b8b78fcd9
RonanBERDY <ronan.berdy3@etu.univ-lorraine.fr>
2025-02-01 19:17:43 +0100

    validators

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms'; //formbuilder c le container de form
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'; //formbuilder c le container de form
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   fb=inject(FormBuilder);
   form=this.fb.nonNullable.group({
-    name:[''],email:[''],password:[''],confirm_password:[''],//if age age:[18]
+    name:['',[Validators.required]],email:['',[Validators.email]],password:['',[Validators.required, Validators.minLength(6)]],confirm_password:[''],//if age age:[18]
   });
 }
