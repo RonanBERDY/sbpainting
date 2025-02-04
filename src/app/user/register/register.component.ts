f0b9b47c82902e7d7f005a0873ae49c853aad2b4
RonanBERDY <ronan.berdy3@etu.univ-lorraine.fr>
2025-02-04 19:47:21 +0100

    submit

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,17 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'; //formbuilder c le container de form
 import { CommonModule } from '@angular/common'; //permet d importer jsonpipe
 import { InputComponent } from '../../shared/input/input.component';
+import { AlertComponent } from "../../shared/alert/alert.component";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule,CommonModule,InputComponent],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent, AlertComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   fb=inject(FormBuilder);
+  showAlert=signal(false);
+  Alertmsg=signal('please wait!');
+  alertColor=signal('blue');
   form=this.fb.nonNullable.group({
     name:['',[Validators.required,Validators.minLength(3)]],email:['',[Validators.email]],password:['',[Validators.required, Validators.pattern(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/)]],confirm_password:['',[Validators.required]],//if age age:[18] + regexr.com pour les pattern
   });
+  register(){
+    this.showAlert.set(true);
+    this.Alertmsg.set('please wait!');
+    this.alertColor.set('blue');
+  }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/**/*.{html,ts}"],
+  safelist: ['bg-blue-400','bg-green-400', 'bg-red-400'],
   theme: {
     extend: {},
     fontFamily:{
         lora:["Lora"],
-    
+
     }
-    
+
   },
 
   plugins: [],
