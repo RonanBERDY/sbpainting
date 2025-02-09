b2c35578ee8d56dbf6819bd07c5975ea62fb2fe0
RonanBERDY <ronan.berdy3@etu.univ-lorraine.fr>
2025-02-09 20:11:28 +0100

    fireauth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
-    provideFirebaseApp(()=>initializeApp())]
+    provideFirebaseApp(()=>initializeApp({
+      apiKey: "AIzaSyAVAGQBFlu46h1MRzhJCEJX1DrVILKGEK8",
+      authDomain: "sbpainting-95531.firebaseapp.com",
+      projectId: "sbpainting-95531",
+      storageBucket: "sbpainting-95531.firebasestorage.app",
+      messagingSenderId: "301100706383",
+      appId: "1:301100706383:web:ed5c30260493993aa5583a",
+      measurementId: "G-5CLXMKN4LX"
+    })
+  ),provideAuth(()=>getAuth()),
+  provideAnalytics(()=>getAnalytics())
+]
 };
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms'; /
 import { CommonModule } from '@angular/common'; //permet d importer jsonpipe
 import { InputComponent } from '../../shared/input/input.component';
 import { AlertComponent } from "../../shared/alert/alert.component";
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-register',
@@ -12,6 +13,7 @@ import { AlertComponent } from "../../shared/alert/alert.component";
 })
 export class RegisterComponent {
   fb=inject(FormBuilder);
+  private auth=inject(Auth); //# = private
   form=this.fb.nonNullable.group({
     name:['',[Validators.required,Validators.minLength(3)]],email:['',[Validators.email]],
     password:['',[Validators.required, Validators.pattern(/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/)]],
@@ -20,10 +22,27 @@ export class RegisterComponent {
   showAlert=signal(false);
   Alertmsg=signal('please wait !');
   alertColor=signal('blue');
-  register(){
+  inSubmission=signal(false)
+  async register(){                   //async car on retourne une promise
     this.showAlert.set(true);
     this.Alertmsg.set('please wait !');
     this.alertColor.set('blue');
+    this.inSubmission.set(true);
+    const {email,password}=this.form.getRawValue();
+    try {
+    const usercred = await createUserWithEmailAndPassword(
+    this.auth,email,password);
+    console.log(usercred)
+    this.Alertmsg.set('Success, your account has been created');
+    this.alertColor.set('green');
+  }
+    catch(e){
+      console.log(e);
+      this.Alertmsg.set('An expected error occured, please try again later');
+      this.alertColor.set('red');
+      this.inSubmission.set(false);
+      return;
+    }
   }
 
 }
