c0f6ced7d1cf228846881538ada137d8e760ea9b
RonanBERDY <ronan.berdy3@etu.univ-lorraine.fr>
2025-02-15 16:13:44 +0100

    routeguard/404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { ManageComponent } from './views/manage/manage.component';
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { UploadComponent } from './views/upload/upload.component';
 import { PaintingsComponent } from './views/paintings/paintings.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 
 export const routes: Routes = [{
   path: '',
@@ -13,14 +16,23 @@ export const routes: Routes = [{
   {path:'about',component:AboutComponent,},
   {path:'manage',
     component:ManageComponent,
-    data: {adminOnly : true }
+    data: {adminOnly : true,
+      authGuardPipe: redirectUnauthorizedToHome,
+     },
+    canActivate:[AuthGuard],
   },
   {path:'upload',
     component:UploadComponent,
-    data: {adminOnly : true }
+    data: {adminOnly : true,
+      authGuardPipe: redirectUnauthorizedToHome,
+     },
+    canActivate:[AuthGuard],
   },
   {path:'paintings/:id',
     component:PaintingsComponent,
 
+  },
+  {path:'**',
+    component:NotfoundComponent,
   }
 ];
--- src/app/views/manage/manage.component.ts
+++ src/app/views/manage/manage.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component,inject, OnInit,signal } from '@angular/core';
+import { RouterLink, Router, ActivatedRoute,Params } from '@angular/router';
 @Component({
   selector: 'app-manage',
   imports: [RouterLink],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
-export class ManageComponent {
-
+export class ManageComponent implements OnInit {
+  router=inject(Router);
+  route=inject(ActivatedRoute);
+  order=signal('1');
+  sort($event: Event){
+    const{value}=$event.target as HTMLSelectElement;
+    this.router.navigate([],{
+      relativeTo:this.route,
+      queryParams:{sort:value}
+    }
+    );
+  }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params:Params)=>{   // this.route.queryParams est un Observable qui “émet” chaque fois que les query params (les paramètres dans l’URL, ex. ?sort=2) changent.
+      this.order.set(params['sort']=== 2 ? '2':'1')
+    });
+  }
 }
