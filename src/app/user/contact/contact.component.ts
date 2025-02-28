4850ec5df48140ae2dc0f6866646044e19b03b17
RonanBERDY <151461086+RonanBERDY@users.noreply.github.com>
2025-02-28 18:59:50 +0100

    end?

--- src/app/user/contact/contact.component.ts
+++ src/app/user/contact/contact.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,signal,inject } from '@angular/core';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
-
+import { FormsModule } from '@angular/forms';
+import { AlertComponent } from '../../shared/alert/alert.component';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input/input.component';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule,AlertComponent,ReactiveFormsModule,InputComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactUsComponent {
+  fb=inject(FormBuilder);
+  form=this.fb.nonNullable.group({
+      user_name:['',[Validators.required,Validators.minLength(3)]],user_email:['',[Validators.email]],
+      //if age age:[18] + regexr.com pour les pattern
+    }
+  );
   public sendEmail(e: Event) {
     e.preventDefault();
 
@@ -23,5 +32,14 @@ export class ContactUsComponent {
           console.log('FAILED...', (error as EmailJSResponseStatus).text);
         },
       );
+      this.showAlert.set(true);
+      this.Alertmsg.set("Successfully sent");
+      this.alertColor.set("green");
   }
+
+  showAlert=signal(false);
+  Alertmsg=signal('');
+  alertColor=signal('');
+  inSubmission=signal(false);
+
 }
