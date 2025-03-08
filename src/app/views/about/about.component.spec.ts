bad8b2ecc997ef06ca1aef2f0fa6c08ede959291
RonanBERDY <151461086+RonanBERDY@users.noreply.github.com>
2025-03-08 12:04:31 +0100

    test

--- src/app/views/about/about.component.spec.ts
+++ src/app/views/about/about.component.spec.ts
@@ -1,23 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from "@angular/core/testing";
+import { AboutComponent } from "./about.component";
 
-import { AboutComponent } from './about.component';
+describe('about-component',()=>{
+  let fixture:ComponentFixture<AboutComponent>;
+  let Component:AboutComponent;
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AboutComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  beforeEach(async()=>{await TestBed.configureTestingModule({
+    imports:[AboutComponent],
+  }).compileComponents();
+});
+beforeEach(()=>{
+  fixture=TestBed.createComponent(AboutComponent);
+  Component=fixture.componentInstance;
+  fixture.autoDetectChanges();
+});
+  it('should create',()=>{expect(Component).toBeTruthy();})
 });
